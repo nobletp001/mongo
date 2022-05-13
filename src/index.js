070b86b0b052dbf1c4b087d04cf15badc001f25e
ekenmapeter <ekenma.peter@gmail.com>
2022-05-13 12:27:24 +0100

    second commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from 'react-dom'; 
+import ReactDOM from "react-dom";
 import Main from "./main";
 import "./customcss/style.css";
+import ReactGA from 'react-ga'; 
 
-
+ ReactGA.initialize('UA-175340939-1');
 ReactDOM.render(
   <Main />, 
   document.getElementById("root")
 );
-
